1086801e055d0fabfbc863d671da134c986c547c
ray-pH <rayhan.azizi9@gmail.com>
2021-01-26 22:16:05 +0700

    haskell

---
+++ haskell.hs
@@ -0,0 +1,74 @@
+import Control.Concurrent
+
+data Cell = Alive | Dead
+    deriving (Show, Eq)
+type Board = [[Cell]]
+
+expandRow :: Int -> [String] -> [String]
+expandRow hei strarr
+    | length strarr >= hei = strarr
+    | otherwise = expandRow hei $ strarr ++ [""]
+
+expandCol :: Int -> String -> String
+expandCol wid str
+    | length str >= wid = str
+    | otherwise = expandCol wid $ str ++ "."
+
+expandStr :: [String] -> Int -> Int -> [String]
+expandStr strarr hei wid = map (expandCol wid) $ expandRow hei strarr
+
+initBoard :: [String] -> Int -> Int -> Board
+initBoard strarr hei wid = map (map toCell) $ expandStr strarr hei wid
+    where toCell :: Char -> Cell
+          toCell c = if c == '#' then Alive else Dead
+
+showBoard :: Char -> Char -> Board -> String
+showBoard aliveChar deadChar board = 
+    combineString $ map (map toChar) board
+        where toChar :: Cell -> Char
+              toChar c = if c == Alive then aliveChar else deadChar
+              combineString :: [String] -> String
+              combineString [] = ""
+              combineString (s:ss) = s ++ "\n" ++ combineString ss
+
+countNeighbors :: Board -> Int -> Int -> Int
+countNeighbors board row col =
+    let height = length board
+        width  = length $ head board
+        neighcoords = [(mod (row+i) height, mod (col+j) width) 
+          | i <- [-1..1], j <- [-1..1], i/=0 || j/=0 ]
+     in length $ filter (== Alive) $ [board!!r!!c | (r,c) <- neighcoords]
+
+rule :: (Int,Cell) -> Cell
+rule (n,cell) 
+    | cell == Alive = if elem n [2,3] then Alive else Dead
+    | cell == Dead  = if n == 3       then Alive else Dead
+
+nextStage :: Board -> Board
+nextStage board = let zipped   = zip [0..] $ map (zip [0..]) board
+                      zipped'  = map concatZip zipped
+                      zipped'' = map (map getNeighbor) zipped' 
+                   in map (map rule) zipped''
+    where concatZip :: (Int, [(Int, Cell)]) -> [(Int,Int,Cell)]
+          concatZip (row, arr) = [(row,col,c) | (col,c) <- arr]
+          getNeighbor :: (Int,Int,Cell) -> (Int,Cell)
+          getNeighbor (row,col,cell) = (countNeighbors board row col, cell)
+
+gol :: Board -> (Int,Int) -> IO()
+gol prev (h,w) = do
+    let next   = nextStage prev 
+        cursor = "\ESC[" ++ show(h) ++ "A"
+    putStr $ cursor ++ showBoard '#' '.' next
+    threadDelay 250000
+    gol next (h,w)
+
+main :: IO()
+main = do
+    let height  = 8
+        width   = 10
+        initStr = [".#.",
+                   "..#",
+                   "###"]
+        initb   = initBoard initStr height width
+    putStr $ showBoard '#' '.' initb 
+    gol initb (height, width)
